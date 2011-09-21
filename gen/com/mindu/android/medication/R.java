a92215d5ef5b90b758c35914406418004a8c1e5b
Gowri Rudrabhatla <gowrikrm@gmail.com>
2011-09-21 19:35:23 +1200

    modified:   bin/MindU.apk 	modified:   bin/classes.dex 	modified:   bin/com/mindu/android/activities/medication/MedicationMainActivity.class 	modified:   bin/com/mindu/android/medication/R$layout.class 	modified:   bin/com/mindu/android/medication/R$string.class 	modified:   bin/resources.ap_ 	modified:   gen/com/mindu/android/medication/R.java 	deleted:    res/layout/add_reminder.xml

--- gen/com/mindu/android/medication/R.java
+++ gen/com/mindu/android/medication/R.java
@@ -25,8 +25,7 @@ public final class R {
     }
     public static final class layout {
         public static final int add_medication=0x7f030000;
-        public static final int add_reminder=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
